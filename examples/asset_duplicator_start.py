4ccd1882ce2068fd39455600a8a31dc8a49fd595
JanPhKoch <janphkoch@gmail.com>
2021-01-24 13:56:00 +0100

    added basis for example + pdf about update news as a pipeline td

---
+++ examples/asset_duplicator_start.py
@@ -0,0 +1,72 @@
+from functools import partial
+import random
+import maya.cmds as cmds
+import pymel.core as pm
+
+
+class MayaAssetDuplicator:
+    def __init__(self):
+        self._create_window()
+
+    def _create_window(self):
+        # Quick and dirty maya.cmds gui because i was to lazy to build a proper one with qt
+
+        window_id = 'Maya Asset Duplicator'
+        if cmds.window(window_id, exists=True):
+            cmds.deleteUI(window_id)
+            cmds.windowPref(window_id, remove=True)
+        window = cmds.window(window_id, title='Maya Asset Duplicator', widthHeight=(350, 350))
+        cmds.frameLayout(label='Options:')
+        cmds.rowColumnLayout(numberOfColumns=3, columnWidth=[(1, 200), (2, 50), (3, 100)])
+        cmds.text(label='Duplicate selected Asset : ')
+        self.duplicator_tf = cmds.textField('duplicator_tf')
+        cmds.text(label=' times.')
+        cmds.setParent('..')
+
+        cmds.rowColumnLayout(numberOfColumns=1, columnWidth=[(1, 350)])
+        cmds.setParent('..')
+        cmds.separator()
+
+        cmds.button(label='D U P L I C A T E', h=50, command=partial(self._get_user_input),
+                    backgroundColor=[random.random(), random.random(), random.random()])
+        cmds.separator()
+
+        cmds.showWindow(window)
+
+    def _get_user_input(self, *args):
+        """
+        Wrapper / helper function when used with the maya gui _create_window to get the user input
+        and parse it to the duplicator.
+        """
+        amount = None  # todo
+        selection = None  # todo
+
+        self.duplicate_asset(selection, amount)
+
+    def identify_asset_typ(self, asset):
+        """
+        Helper function to identify how to handle a specific asset based on the way it was imported.
+        """
+        # reference
+        reference_node = None  # todo
+
+    def duplicate_asset(self, asset, amount):
+        """
+        Duplicates the given asset by the given amount based on the identified asset typ.
+
+        Args:
+            asset: Asset selection user input.
+            amount: int amount user input to duplicate the asset by.
+
+        Returns:
+
+        """
+        asset_typ = self.identify_asset_typ(asset)
+
+        node_type = None
+        reference_node = None
+        reference_namespace = None
+
+
+if __name__ == '__main__':
+    MayaAssetDuplicator()
